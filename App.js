1fa472a18d55257dc51620fe734490ee9aefdf5a
Faris Halteh <faris.halteh@gmail.com>
2023-10-02 18:31:23 +0200

    exercise-1

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 16,
+  },
+  sectionTitle: {
+    fontSize: 34,
+    fontWeight: "bold",
   },
 });
