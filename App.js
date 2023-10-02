53e83077de697f31f11f0765bf80f0f8113e277e
Faris Halteh <faris.halteh@gmail.com>
2023-10-02 20:53:45 +0200

    exercise-2

--- App.js
+++ App.js
@@ -5,6 +5,14 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's Tasks</Text>
+        <View style={styles.items}>
+          <View style={styles.item}><Text style={styles.itemText}>Task 1</Text></View>
+          <View style={styles.item}><Text style={styles.itemText}>Task 2</Text></View>
+          <View style={styles.item}><Text style={styles.itemText}>Task 3</Text></View>
+          <View style={styles.item}><Text style={styles.itemText}>Task 4</Text></View>
+          <View style={styles.item}><Text style={styles.itemText}>Task 5</Text></View>
+          <View style={styles.item}><Text style={styles.itemText}>Task 6</Text></View>
+        </View>
       </View>
     </View>
   );
@@ -23,4 +31,14 @@ const styles = StyleSheet.create({
     fontSize: 34,
     fontWeight: "bold",
   },
+  items: {
+    marginTop: 32,
+  },
+  item: {
+    marginBottom: 16,
+  },
+  itemText: {
+    fontSize: 17,
+    maxWidth: "80%",
+  }
 });
